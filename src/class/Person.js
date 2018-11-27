9316ea5a2c44ed79467c4c07e85cde0194176818
qoov <emrys8128@gmail.com>
2018-11-27 21:55:30 +0900

    Person

---
+++ src/class/Person.js
@@ -0,0 +1,17 @@
+class Person {
+  constructor(name, surname, age) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+  }
+
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+
+  static older(person1, person2) {
+    return (person1.age >= person2.age) ? person1 : person2;
+  }
+}
+
+console.log(new Person());
