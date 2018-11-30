a0e53372458bad0c2d4e2cf1167f1c64e1e14239
qoov <emrys8128@gmail.com>
2018-11-30 21:13:52 +0900

    Promise

--- src/Promise/promise.js
+++ src/Promise/promise.js
@@ -14,3 +14,23 @@ promise
   .catch((error) => {
     console.error(error);
   });
+
+promise
+  .then((message) => {
+    console.log('0', message);
+    return new Promise((resolve, reject) => {
+      resolve(message);
+    });
+  })
+  .then((message2) => {
+    console.log('1', message2);
+    return new Promise((resolve, reject) => {
+      resolve(message2);
+    });
+  })
+  .then((message3) => {
+    console.log('2', message3);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
