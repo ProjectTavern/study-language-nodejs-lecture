6c353b1825b5d692c9782386df5000ada1186319
qoov <emrys8128@gmail.com>
2018-11-30 21:06:30 +0900

    Promise

---
+++ src/Promise/promise.js
@@ -0,0 +1,16 @@
+const condition = true;
+const promise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('success');
+  } else {
+    reject('failed');
+  }
+});
+
+promise
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
